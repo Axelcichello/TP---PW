f66358d1998f62bd2cd86008235382824e7c8495
axelcichello <csaxel49@gmail.com>
2024-04-16 20:14:05 -0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+
+$aldeano = new Aldeano;
+$arbusto = new Arbusto;
+
+$aldeano->recolectar($arbusto);
+// Se debe ver el echo que devuelve el método recolectar
+
+$pesquero = new Pesquero;
+$bancoDePesca = new BancoDePesca;
+
+$pesquero->recolectar($bancoDePesca);
+// Se debe ver el echo que devuelve el método recolectar
+
+// ------
+
+$aldeanoChino = new AldeanoChino;
+$aldeanoFranco = new AldeanoFranco;
+$arbusto1 = new Arbusto;
+$arbusto2 = new Arbusto;
+
+$aldeanoChino->recolectar($arbusto1);
+// Se debe ver el echo que devuelve el método recolectar sin bonus
+
+$aldeanoFranco->recolectar($arbusto2);
+// Se debe ver el echo que devuelve el método recolectar con el bonus del 25%
